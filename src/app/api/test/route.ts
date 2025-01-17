37897ca980c9dc442e7ca1ca539f31672d3c4def
Pablo Ruiz <pablo_ruiza@hotmail.com>
2025-01-17 12:19:38 -0500

    feat: configuration of prisma client, now requests can be made to the api while running in docker

---
+++ src/app/api/test/route.ts
@@ -0,0 +1,27 @@
+import { NextResponse } from 'next/server';
+import  {prisma}  from '@/lib/prisma'
+
+export async function POST(request: Request) {
+    try{
+        const {title} = await request.json();
+
+        const newNote = await prisma.test.create({
+            data: {
+                title,
+
+            }
+        });
+    
+        return NextResponse.json(newNote)
+    }catch(error){
+        if(error instanceof Error){
+            return NextResponse.json({
+                message: error.message
+            }, {
+                status: 500
+            })
+        }
+        
+    }
+    
+}
