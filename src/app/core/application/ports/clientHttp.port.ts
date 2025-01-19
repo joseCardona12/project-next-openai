252974effd0cfd4a9ff3c669924a8328064e4b43
joseCardona12 <josesiprozmater@gmail.com>
2025-01-19 15:49:30 -0500

    docs: Add docs in clientHttp.port

--- src/app/core/application/ports/clientHttp.port.ts
+++ src/app/core/application/ports/clientHttp.port.ts
@@ -6,12 +6,14 @@ export interface ClientHttpPort{
      * @param headers this headers for send to fetch 
      * @param method simple method as GET, POST, PUT, DELETE
      * @param bodyClient Body the request
+     * This method return the completed response or throw new Error
      */
     fetchApi<B>(path:string,headers:Record<string,string>, method:string,bodyClient?:B):Promise<Response>,
 
     /**
      * 
      * @param path this property is a only resource for access
+     * This method get all items, for example: users, 
      */
     get<T>(path:string):Promise<T>,
     
@@ -19,6 +21,7 @@ export interface ClientHttpPort{
      * 
      * @param path this property is a only resource for access
      * @param body Body the request
+     * This method create an item
      */
     post<T,B>(path:string,body:B):Promise<T>
 
@@ -26,12 +29,14 @@ export interface ClientHttpPort{
      * 
      * @param path this property is a only resource for access
      * @param body Body the request
+     * This method update an item
      */
     put<T,B>(path:string,body:B):Promise<T>,
 
     /**
      * 
      * @param path this property is a only resource for access
+     * This method delete an item
      */
     delete<T>(path:string):Promise<T>
 }
