72d4554b3395371415887ee60f41d9b3bbb3fd65
joseCardona12 <josesiprozmater@gmail.com>
2025-01-19 20:16:34 -0500

    feat: Add context user

--- src/app/(main)/(private)/context/page.tsx
+++ src/app/(main)/(private)/context/page.tsx
@@ -1,11 +1,14 @@
-"use client"
+"use client";
+import { useContextState } from "@/app/core/application/global-state/context.state";
 import ContextPush from "@/ui/molecules/contextpush/ContextPush";
 import DataCollection from "@/ui/organisms/dataCollection/DataCollection";
 
-export default function ContextView(){
-    return (
-        <div className="containergeneral">
-            <ContextPush/>
-        </div>
-    )
+export default function ContextView() {
+  const { contextState } = useContextState((state) => state);
+  console.log("context", contextState);
+  return (
+    <div className="containergeneral">
+      <ContextPush />
+    </div>
+  );
 }
