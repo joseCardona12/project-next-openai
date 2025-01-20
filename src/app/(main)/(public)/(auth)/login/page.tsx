610a01456146cd91bff1ea32e86451eb57e0d645
joseCardona12 <josesiprozmater@gmail.com>
2025-01-19 19:25:44 -0500

    feat: Add context state

--- src/app/(main)/(public)/(auth)/login/page.tsx
+++ src/app/(main)/(public)/(auth)/login/page.tsx
@@ -3,7 +3,7 @@ import { LoginTemplate } from "@/ui/template/LoginTemplate";
 
 export default function LoginView() {
   OpenAiService.createPrompt(
-    "Mi objetivo es bajar de peso, soy sedentario, tengo entre 19-29 años, quiero 3-4 dias en la semana, puedo dedciar a la session 30min. Necesito que me respondas los ejercicios en un formato json con las propiedades: name, numberSecond,series, urlimageRandom"
+    "Mi objetivo es bajar de peso, soy sedentario, tengo entre 19-29 años, quiero 3-4 dias en la semana, puedo dedciar a la session 30min. Necesito que me respondas los ejercicios en un formato json con las propiedades: name, numberSecond,series, imageUrlRandom esta propiedad debe ser una rl random para ver la imagen"
   );
   return <LoginTemplate />;
 }
