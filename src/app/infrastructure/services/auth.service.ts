6c281633c0abb14d206589bf807ef1e7b0da9ea2
joseCardona12 <josesiprozmater@gmail.com>
2025-01-22 12:58:45 -0500

    feat: Add authService

--- src/app/infrastructure/services/auth.service.ts
+++ src/app/infrastructure/services/auth.service.ts
@@ -1,24 +1,20 @@
-import { ILoginRequest, ILoginResponseError, ILoginResponseErrorCatch, ILoginResponseSuccess, IRegisterRequest, IRegisterResponse } from "@/app/core/application/dto";
+import { ILoginRequest, ILoginResponseError, ILoginResponseErrorCatch, ILoginResponseSuccess, IRegisterRequest, IRegisterResponseError, IRegisterResponseErrorCatch, IRegisterResponseSuccess } from "@/app/core/application/dto";
 import { AuthPort } from "@/app/core/application/ports";
 import { ClientHttpUtil } from "@/app/infrastructure/utils/clientHttp.util";
 
-class AuthService {
+class AuthService implements AuthPort{
     private clientHttpUtil:ClientHttpUtil;
 
     constructor(){
         this.clientHttpUtil = new ClientHttpUtil();
     };
     public async login(data: ILoginRequest): Promise<ILoginResponseError | ILoginResponseErrorCatch | ILoginResponseSuccess> {
-        if(!data){
-            return {error:"email and password are required", status:400};
-        }
         return await this.clientHttpUtil.post<ILoginResponseError | ILoginResponseErrorCatch | ILoginResponseSuccess,ILoginRequest >("auth/login", data);
     };
 
-    // public async register(data: IRegisterRequest): Promise<ILoginResponseError | ILoginResponseErrorCatch | IRegisterResponse> {
-        
-    // };
-
+    public async register(data: IRegisterRequest): Promise<IRegisterResponseError | IRegisterResponseErrorCatch | IRegisterResponseSuccess> {
+        return await this.clientHttpUtil.post<IRegisterResponseError | IRegisterResponseErrorCatch | IRegisterResponseSuccess, IRegisterRequest>("auth/register", data);
+    };
 };
 
 export default new AuthService();
