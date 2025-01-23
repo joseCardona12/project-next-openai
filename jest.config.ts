8e57a02f7edf553e24e4c4fdf8d8ca6cd8dc25c3
Pablo Ruiz <pablo_ruiza@hotmail.com>
2025-01-23 14:18:15 -0500

    feat: register test is done. AB-72-73

---
+++ jest.config.ts
@@ -0,0 +1,26 @@
+import type { Config } from 'jest';
+
+const config: Config = {
+  clearMocks: true,
+  collectCoverage: true,
+  coverageDirectory: 'coverage',
+  coverageProvider: 'v8',
+  transform: {
+    '^.+\\.(ts|tsx)$': 'ts-jest',
+  },
+  transformIgnorePatterns: [
+    '/node_modules/',
+  ],
+  moduleFileExtensions: ['js', 'jsx', 'ts', 'tsx', 'json', 'node'],
+  testEnvironment: 'node',
+  globals: {
+    'ts-jest': {
+      isolatedModules: true,
+    },
+  },
+  moduleNameMapper: {
+    '^@/(.*)$': '<rootDir>/src/$1', // Mapea el alias `@/` a la carpeta `src/` de tu proyecto
+  },
+};
+
+export default config;
